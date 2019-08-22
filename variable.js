f3c7ec3354797b9ccd8335497c49deb877c1eb13
nischal <nischalbohara77@gmail.com>
2019-08-22 21:22:02 +0545

    Adding Variable.js

---
+++ variable.js
@@ -0,0 +1,11 @@
+let a = 1;
+let b = 2;
+let c="nischal";
+let d=2.2;
+
+console.log(a + b);
+console.log(a+d);
+console.log(c);
+
+//After finding string , it start to concatenate.
+console.log(a+b+c+d+a);
